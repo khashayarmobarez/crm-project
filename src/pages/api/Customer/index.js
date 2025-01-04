b47bee5c75a9d475aad5c36bd6bc2b3cd48c9372
khashayar <khashayarmobarezbusiness@gmail.com>
2025-01-04 21:15:38 +0330

    connect to db

--- src/pages/api/Customer/index.js
+++ src/pages/api/Customer/index.js
@@ -11,18 +11,20 @@ export default async function handler(req,res) {
         return
     }
 
+    if (req.method !== 'POST') {
+        return res.status(405).json({ status: 'failed', message: 'Method not allowed' });
+    }
+
     if (req.method === 'POST' ) {
         const data = req.body.data
 
         if(!data.name || !data.lastName || !data.email) 
-            return
-            res.status(400).json({status: 'failed', message: 'invalid data'})
-
+            return res.status(400).json({status: 'failed', message: 'invalid data'})
         try {
             const customer = await Customer.create(data)
-            res.status(201).json({status: 'success', message: 'Data created', data: customer}) 
+            return res.status(201).json({status: 'success', message: 'Data created', data: customer}) 
         } catch(err) { 
-            res.status(500).json({status: 'failed', message: 'error in storing data in database'})
+            return res.status(500).json({status: 'failed', message: 'error in storing data in database', error: err.message})
         }
     }   
 
