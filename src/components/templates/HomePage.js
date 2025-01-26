86f825ddd19558e7be7041637d34d8062ae17fc7
khashayar <khashayarmobarezbusiness@gmail.com>
2025-01-26 13:52:58 +0330

    Implement server-side rendering for customer data and create HomePage component

---
+++ src/components/templates/HomePage.js
@@ -0,0 +1,11 @@
+
+
+export default function HomePage({ customers }) {
+
+  return (
+    <div>
+      
+    </div>
+  )
+  
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
 
+// pages
+import HomePage from "@/components/templates/HomePage";
 
-export default function Home() {
+// ssr page
+export default function Index({ customers}) {
+
+  console.log(customers);
+
   return (
-    <div className="w-full flex flex-col items-center text-white">
-      <p>test</p>
-    </div>
+    <HomePage customers={customers} />
   );
+}
+
+
+export async function getServerSideProps() {
+
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      }
+    }
+  } catch(err) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {},
+  };
 }
