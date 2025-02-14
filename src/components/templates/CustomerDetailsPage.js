b66618a8ba6860849bff8a71224e3c5f91dac631
khashayar <khashayarmobarezbusiness@gmail.com>
2025-02-15 00:53:33 +0330

    finished crm project

--- src/components/templates/CustomerDetailsPage.js
+++ src/components/templates/CustomerDetailsPage.js
@@ -1,11 +1,70 @@
+import moment from "moment";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 
 const CustomerDetailsPage = ({data}) => {
 
-    console.log(data)
+
+    const router = useRouter()
+
+    const deleteHandler = async (userData) => {
+
+
+        const res = await fetch(`/api/deleteCustomer/${userData._id}`, {
+            method: 'DELETE'
+        })
+
+        const data = await res.json()
+        if(data.status === 'success') {
+            router.push('/')
+        }
+    }
 
     return (
-        <div>
-            
+        <div className="customer-details">
+            <h4>customer details</h4>
+            <div className="customer-detail__main">
+                <div className="customer-detail__item">
+                    <span>name: </span>
+                    <p>{data.name}</p>
+                </div>
+                <div className="customer-detail__item">
+                    <span>last name: </span>
+                    <p>{data.lastName}</p>
+                </div>
+                <div className="customer-detail__item">
+                    <span>phone: </span>
+                    <p>{data.phone}</p>
+                </div>
+                <div className="customer-detail__item">
+                    <span>postal code: </span>
+                    <p>{data.postalCode}</p>
+                </div>
+                <div className="customer-detail__item">
+                    <span>date: </span>
+                    <p>{moment(data.date).utc().format('YYYY-MM-DD')}</p>
+                </div>
+            </div>
+            <div className="customer-detail__products">
+                <p>name</p>
+                <p>price</p>
+                <p>qty</p>
+                {
+                    data.products.map((product, index) => (
+                        <React.Fragment key={index}>
+                            <span>{product.name}</span>
+                            <span>{product.price}</span>
+                            <span>{product.qty}</span>
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+            <div className="customer-detail__buttons">
+                <p>Edit or Delete?</p>
+                <button onClick={() => deleteHandler(data)}>delete</button>
+                <Link href={`/edit/${data._id}`} >edit handler</Link>
+            </div>
         </div>
     );
 };
